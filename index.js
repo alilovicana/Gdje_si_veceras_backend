f0ae2e1d1fe5a29e4500d6aed6115248726dedb6
Alilovic Ana <alilovic220799@gmail.com>
2022-11-26 13:28:10 +0100

    add email check

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')//simple express server
-const app = express()
 const mysql = require('mysql')
-const cors=require('cors')
+const cors = require('cors')
+const app = express()
 
 app.use(cors());
 app.use(express.json());
@@ -17,7 +17,82 @@ app.post("/Registration", (req, res) => {
     const lastName = req.body.lastName;
     const email = req.body.email;
     const password = req.body.password;
-    db.query("INSERT INTO users(firstName,lastName,email,password) VALUES (?,?,?,?)", [firstName, lastName, email, password],
+
+    (db.query("INSERT INTO users(firstName,lastName,email,password) VALUES (?,?,?,?)", [firstName, lastName, email, password],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+            else {
+                res.send("Values Inserted")
+            }
+        }
+    ))
+
+});
+// app.post("/Registration", (req, res) => {
+//     const email = req.body.email;
+//     (db.query(
+//         "SELECT* FROM users WHERE email = ?;",
+//         [email],
+//         (err, result) => {
+//             if (err) {
+//                 res.send({ err: err });
+//             }
+//             if (result.length > 0) {
+//                 res.send({ message: "Korisnik već postoji postoji, pokušajte ponovno!" });
+//             } else {
+//                 res.send(result);
+//             }
+//         }
+
+
+//     ));
+
+// });
+// app.get("/Login", (req, res) => {
+//     if (req.session.user) {
+//       res.send({ loggedIn: true, user: req.session.user });
+//     } else {
+//       res.send({ loggedIn: false });
+//     }
+//   });
+
+app.post("/Login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    (db.query(
+        "SELECT* FROM users WHERE email = ?;",
+        [email],
+        (err, result) => {
+            if (err) {
+                res.send({ err: err });
+            }
+
+            if (result.length > 0) {
+                // bcrypt.compare(password, result[0].password, (error, response) => {
+                //     if (response) {
+                //         req.session.user = result;
+                //         console.log(req.session.user);
+                res.send(result);
+                // } else {
+                //     res.send({ message: "Pogrešan unos! Pokušajte ponovo" });
+                // }
+                // });
+            } else {
+                res.send({ message: "Korisnik ne postoji" });
+            }
+        }
+
+
+    ));
+});
+app.post('/CreateAds', (req, res) => {
+    //const user_id=req.body.user_id;
+    const content = req.body.content;
+    const adress = req.body.adress;
+    db.query("INSERT INTO posts(content,adress) VALUES (?,?)", [content, adress],
         (err, result) => {
             if (err) {
                 console.log(err)
@@ -27,33 +102,33 @@ app.post("/Registration", (req, res) => {
             }
         }
     );
-});
-app.post('/CreateAds', (req, res) => {
-    // const user_id=req.body.user_id;
-    const content = req.body.content;
-    const adress = req.body.adress;
-    db.query("INSERT INTO posts(content,adress) VALUES (?,?)",[content,adress],
-    (err,result)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.send("Values Inserted")
-        }
-    }
-    );
 
 });
 app.get('/showAds', (req, res) => {
     db.query("SELECT* FROM posts",
-    (err,result)=>{
-        if(err){
-            console.log(err)
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+            else {
+                res.send(result)
+            }
+        })
+});
+
+app.put('/update', (req, res) => {
+    const id = req.body.id;
+    const likes = req.body.likes;
+    db.query("UPDATE posts SET likes=? WHERE id=?", [likes, id],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+            else {
+                res.send("Values Inserted")
+            }
         }
-        else{
-            res.send(result)
-        }
-    })
+    );
 });
 app.listen(3001, () => {
     console.log("jej your serveer is running!");
