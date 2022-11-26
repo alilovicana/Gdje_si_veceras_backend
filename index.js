c86cb35264a34ec192f40f46c28c4406809bda6e
Alilovic Ana <alilovic220799@gmail.com>
2022-11-26 16:31:08 +0100

    add a cookie and a session

--- index.js
+++ index.js
@@ -3,9 +3,36 @@ const mysql = require('mysql')
 const cors = require('cors')
 const app = express()
 
-app.use(cors());
-app.use(express.json());
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+app.use(express.json());
+app.use(
+    cors({
+      origin: ["http://localhost:3400"],
+      methods: ["GET", "POST"],
+      credentials: true,
+    })
+  );
+  app.use(cookieParser());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  
+  app.use(
+    session({
+      key: "userId",
+      secret: "vitez",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        expires: 1000 * 60 * 25 * 60 ,
+      },
+    })
+  );
+  
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
@@ -18,17 +45,21 @@ app.post("/Registration", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    (db.query("INSERT INTO users(firstName,lastName,email,password) VALUES (?,?,?,?)", [firstName, lastName, email, password],
-        (err, result) => {
-            if (err) {
-                console.log(err)
-            }
-            else {
-                res.send("Values Inserted")
-            }
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+            console.log(err);
         }
-    ))
-
+        db.query("INSERT INTO users(firstName,lastName,email,password) VALUES (?,?,?,?)", [firstName, lastName, email, hash],
+            (err, result) => {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    res.send("Values Inserted")
+                }
+            }
+        )
+    });
 });
 // app.post("/Registration", (req, res) => {
 //     const email = req.body.email;
@@ -45,18 +76,15 @@ app.post("/Registration", (req, res) => {
 //                 res.send(result);
 //             }
 //         }
-
-
 //     ));
-
 // });
-// app.get("/Login", (req, res) => {
-//     if (req.session.user) {
-//       res.send({ loggedIn: true, user: req.session.user });
-//     } else {
-//       res.send({ loggedIn: false });
-//     }
-//   });
+app.get("/Login", (req, res) => {
+    if (req.session.user) {
+      res.send({ loggedIn: true, user: req.session.user });
+    } else {
+      res.send({ loggedIn: false });
+    }
+  });
 
 app.post("/Login", (req, res) => {
     const email = req.body.email;
@@ -71,15 +99,15 @@ app.post("/Login", (req, res) => {
             }
 
             if (result.length > 0) {
-                // bcrypt.compare(password, result[0].password, (error, response) => {
-                //     if (response) {
-                //         req.session.user = result;
-                //         console.log(req.session.user);
-                res.send(result);
-                // } else {
-                //     res.send({ message: "Pogrešan unos! Pokušajte ponovo" });
-                // }
-                // });
+                bcrypt.compare(password, result[0].password, (error, response) => {
+                    if (response) {
+                        req.session.user = result;
+                        console.log(req.session.user);
+                        res.send(result);
+                    } else {
+                        res.send({ message: "Pogrešan unos! Pokušajte ponovo" });
+                    }
+                });
             } else {
                 res.send({ message: "Korisnik ne postoji" });
             }
